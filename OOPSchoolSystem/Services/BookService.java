1d662a01f55e29c2304dbce464ed5d18b6935a66
bader <bader5uk@hotmail.com>
2024-11-12 11:58:34 +0400

    Added assign book function to assign books to student

--- OOPSchoolSystem/Services/BookService.java
+++ OOPSchoolSystem/Services/BookService.java
@@ -12,7 +12,7 @@ public class BookService {
     static Scanner scanner = new Scanner(System.in);
     static List<Book> bookList = new ArrayList<>();
 
-    public static Book addBook() {
+    public static Book addBook(List<School> schools) {
         Boolean exitFlag = true;
         Book book = new Book();
         System.out.println("Enter book name");
@@ -37,6 +37,7 @@ public class BookService {
                 book.setAvailable(true);
                 exitFlag = false;
             } else if (availableBook.contains("N") || availableBook.contains("n")) {
+                book.setStudentName(assignBook(schools));
                 book.setAvailable(false);
                 exitFlag = false;
             } else {
@@ -46,13 +47,13 @@ public class BookService {
         return book;
     }
 
-    public static List<Book> addBooks(List<School> schoolList) {
+    public static List<Book> addBooks(List<School> schools) {
         Boolean exitFlag = true;
         while (exitFlag) {
             System.out.println("Enter books? Y/N ");
             String newDataEntry = scanner.nextLine();
             if (newDataEntry.contains("Y") || newDataEntry.contains("y")) {
-                bookList.add(BookService.addBook());
+                bookList.add(BookService.addBook(schools));
             } else if (newDataEntry.contains("N") || newDataEntry.contains("n")) {
                 exitFlag = false;
             } else {
@@ -62,7 +63,7 @@ public class BookService {
         return bookList;
     }
 
-    public static void retrieveBook() {
+    public static void retrieveBook(List<School> schools) {
         System.out.println("Enter Book ID: ");
         Integer bookId = scanner.nextInt();
         for (Book book : bookList) {
@@ -73,15 +74,23 @@ public class BookService {
             }
         }
     }
-    public static void assignBook(List<Student> studentList){
+
+    public static String assignBook(List<School> schools) {
+        if (schools.isEmpty()) {
+            System.out.println("There is no student data to use");
+            StudentService.addStudents();
+        }
         System.out.println("Enter Student ID: ");
         String studentId = scanner.nextLine();
-        for(Student student : studentList){
-            if(student.getId().equals(studentId)){
-                System.out.println(student);
-            }else {
-                System.out.println("Student does not exist");
+        for (School school : schools) {
+            for (Student student : school.getStudents()) {
+                if (student.getId().equals(studentId)) {
+                    return studentId;
+                } else {
+                    System.out.println("Student does not exist");
+                }
             }
         }
+        return studentId;
     }
 }
